620c6a80e7a3219819768852749a1e0ed1e01817
young-st511 <asdk511@gmail.com>
2023-02-11 16:44:45 +0900

    💄 style(SignIn): Style SignInForm

--- src/features/auth/components/SignInForm.tsx
+++ src/features/auth/components/SignInForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { FormEventHandler, useState } from "react";
 import * as S from "../../../components/styles/FormStyle";
 import { useAuth } from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
@@ -8,7 +8,9 @@ function SignInForm() {
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
   const { signIn, signInError } = useAuth();
-  const handleSubmit = async () => {
+  const handleSubmit: FormEventHandler = async (e) => {
+    e.preventDefault();
+    
     const isSuccess = await signIn({ email, password });
 
     if (isSuccess) {
@@ -17,23 +19,36 @@ function SignInForm() {
   };
 
   return (
-    <S.Form onSubmit={handleSubmit}>
-      <S.Input
-        value={email}
-        data-testid="email-input"
-        onChange={({ currentTarget }) => setEmail(currentTarget.value)}
-      />
-      <S.Input
-        value={password}
-        data-testid="password-input"
-        onChange={({ currentTarget }) => setPassword(currentTarget.value)}
-      />
-      <S.Error>{signInError}</S.Error>
+    <S.Box>
+      <S.Title>Sign In</S.Title>
+      <S.Form onSubmit={handleSubmit}>
+        <S.Label isError={!!signInError}>
+          <S.LabelTitle>이메일</S.LabelTitle>
+          <S.Input
+            value={email}
+            data-testid="email-input"
+            onChange={({ currentTarget }) => setEmail(currentTarget.value)}
+            required
+          />
+        </S.Label>
+        <S.Label isError={!!signInError}>
+          <S.LabelTitle>비밀번호</S.LabelTitle>
+          <S.Input
+            value={password}
+            type="password"
+            data-testid="password-input"
+            onChange={({ currentTarget }) => setPassword(currentTarget.value)}
+            required
+          />
+        </S.Label>
 
-      <S.SubmitButton type="submit" data-testid="signin-button">
-        Sign In
-      </S.SubmitButton>
-    </S.Form>
+        {signInError ? <S.Error>{signInError}</S.Error> : null}
+
+        <S.SubmitButton type="submit" data-testid="signin-button">
+          Sign In
+        </S.SubmitButton>
+      </S.Form>
+    </S.Box>
   );
 }
 
