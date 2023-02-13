ef3c28e8d118f337e22fe58a7fdd0ba6882fc8ca
young-st511 <asdk511@gmail.com>
2023-02-13 22:33:39 +0900

    🐛 Fix(API): Add API BaseURL on start

--- src/utils/axios/setAxiosDefaultConfig.ts
+++ src/utils/axios/setAxiosDefaultConfig.ts
@@ -1,6 +1,13 @@
 import axios from "axios";
 import { API_BASE_URL } from "../../api/ApiConstants";
+import { getAccessToken } from "../token/accessTokenUtils";
+import { getAuth } from "../token/getAuth";
 
 export function setAxiosDefaultConfig() {
+  const auth = getAuth();
   axios.defaults.baseURL = API_BASE_URL;
+
+  if (auth) {
+    axios.defaults.headers.common.Authorization = "Bearer " + getAccessToken();
+  }
 }
